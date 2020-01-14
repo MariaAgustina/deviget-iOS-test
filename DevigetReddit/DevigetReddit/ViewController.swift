90fa749ffe822cf7174e4a63ce845371e9c5ee41
agustina markosich <agustina.markosich@mercadolibre.com>
2020-01-13 22:43:04 -0300

    *add empty project *add gitignore

---
+++ DevigetReddit/DevigetReddit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevigetReddit
+//
+//  Created by agustina markosich on 1/13/20.
+//  Copyright © 2020 agustina markosich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
